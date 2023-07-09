7395cbcda7805a743adc21ef7905057836ea0732
Boudjema DJEBARRI <117278502+BoudjemaDJEBARRI@users.noreply.github.com>
2023-07-09 20:54:06 +0200

    V1

--- Service-company/src/main/java/com/djeb/entities/Company.java
+++ Service-company/src/main/java/com/djeb/entities/Company.java
@@ -20,40 +20,5 @@ public class Company {
 	private Long id;
 	private String name;
 	private double price;
-	
-	
-	
-	public Company() {
-		super();
-	}
-	public Company(Long id, String name, double price) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.price = price;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	
-	@Override
-	public String toString() {
-		return "Company [id=" + id + ", name=" + name + ", price=" + price + "]";
-	}
-	
+
 }
